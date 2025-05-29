9b3df64de9c4292edabb2d33e8ca46488e210dad
kauaneleeal <kgoncalvesleal@outlook.com>
2025-05-28 23:35:34 -0300

    feat: Recupera nome, transações e extrato do localStorage

--- src/app/(dashboard)/investments/page.tsx
+++ src/app/(dashboard)/investments/page.tsx
@@ -2,47 +2,15 @@ import { GreetingCard } from "@/components/DashboardComponents";
 import { ExtractList } from "@/components/DashboardComponents";
 import InvestmentSection from "@/components/InvestmentSection";
 
-const extracts = [
-  {
-    id: crypto.randomUUID(),
-    month: "Novembro",
-    type: "Deposito",
-    value: 150,
-    date: "18/11/2022",
-  },
-  {
-    id: crypto.randomUUID(),
-    month: "Novembro",
-    type: "Deposito",
-    value: 100,
-    date: "21/11/2022",
-  },
-  {
-    id: crypto.randomUUID(),
-    month: "Novembro",
-    type: "Deposito",
-    value: 50,
-    date: "21/11/2022",
-  },
-  {
-    id: crypto.randomUUID(),
-    month: "Novembro",
-    type: "Transferência",
-    value: -500,
-    date: "21/11/2022",
-  },
-];
-
-
 export default function Home() {
 
   return (
     <div className="w-full flex flex-col xl:flex-row col-span-5 gap-4">
       <div className="flex flex-col flex-wrap flex-1 gap-4">
-        <GreetingCard name="Joana" date="Quinta-feira, 08/09/2024" />
+        <GreetingCard />
         <InvestmentSection />
       </div>
-      <ExtractList extracts={extracts} />
+      <ExtractList />
     </div>
   );
 }
